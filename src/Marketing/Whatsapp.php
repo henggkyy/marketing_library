b65706882ce0c0bb4f8ecb4e0732f60442f2310e
Hengky <henggkyy@gmail.com>
2021-07-21 09:15:00 +0700

    [ADD] Session and QR

--- src/Marketing/Whatsapp.php
+++ src/Marketing/Whatsapp.php
@@ -29,4 +29,76 @@ class Whatsapp{
         }
         return $device_status;
     }
+
+    public function synchronizeSession(){
+        $request = new SendRequest($this->base_url, $this->app_id, $this->api_key);
+        $data = array();
+        $endpoint = 'wa/device/session/sync';
+        $method = 'POST';
+
+        $sync_session = $request->sendRequest($data, null, $endpoint, $method);
+        if($sync_session['error'] === true){
+            throw new Exception($sync_session['message']);
+        }
+        return $sync_session;
+    }
+
+    public function rebootSession(){
+        $request = new SendRequest($this->base_url, $this->app_id, $this->api_key);
+
+        $data = array();
+        $endpoint = 'wa/device/session/reboot';
+        $method = 'POST';
+
+        $reboot_session = $request->sendRequest($data, null, $endpoint, $method);
+        if($reboot_session['error'] === true){
+            throw new Exception($reboot_session['message']);
+        }
+        return $reboot_session;
+    }
+
+    public function resetSession(){
+        $request = new SendRequest($this->base_url, $this->app_id, $this->api_key);
+        $data = array(
+            "wait" => true
+        );
+        $endpoint = 'wa/device/session/reset';
+        $method = 'POST';
+
+        $reset_session = $request->sendRequest($data, null, $endpoint, $method);
+        if($reset_session['error'] === true){
+            throw new Exception($reset_session['message']);
+        }
+        return $reset_session;
+    }
+
+    public function getQrImage(){
+        $request = new SendRequest($this->base_url, $this->app_id, $this->api_key);
+
+        $data = array(
+            "force" => true,
+        );
+        $endpoint = 'wa/qr/get';
+        $method = 'POST';
+
+        $qr_image = $request->sendRequest($data, null, $endpoint, $method);
+        if($qr_image['error'] === true){
+            throw new Exception($qr_image['message']);
+        }
+        return $qr_image;
+    }
+
+    public function getSessionHealth(){
+        $request = new SendRequest($this->base_url, $this->app_id, $this->api_key);
+
+        $data = array();
+        $endpoint = 'wa/device/session/health';
+        $method = 'POST';
+
+        $session_health = $request->sendRequest($data, null, $endpoint, $method);
+        if($session_health['error'] === true){
+            throw new Exception($session_health['message']);
+        }
+        return $session_health;
+    }
 }
